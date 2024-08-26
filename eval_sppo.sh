b059020d2f36f8d472ff8d9cfcd76f181c71cd54
xukp20 <xukp20@mails.tsinghua.edu.cn>
2024-08-26 11:00:18 +0800

    update:add testing for base llama3

--- eval_sppo.sh
+++ eval_sppo.sh
@@ -2,5 +2,10 @@ set -x
 set -e
 
 export IS_ALPACA_EVAL_2=False
+export HF_HOME="/cephfs/shared/hf_cache"
 
-alpaca_eval evaluate_from_model SPPO-Llama-3-Instruct-8B-Iter1-custom 
+REFERENCE_MODEL="gpt4_turbo"
+
+# alpaca_eval evaluate_from_model SPPO-Llama-3-Instruct-8B-Iter1-custom --reference_outputs $REFERENCE_MODEL
+# alpaca_eval evaluate_from_model Meta-Llama-3-8B-Instruct --reference_outputs $REFERENCE_MODEL
+alpaca_eval evaluate_from_model Meta-Llama-3-8B-Instruct-custom --reference_outputs $REFERENCE_MODEL
