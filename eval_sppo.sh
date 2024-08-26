ef727e731a1da634c37cc38106c65cd04a80ab31
xukp20 <xukp20@mails.tsinghua.edu.cn>
2024-08-26 21:38:50 +0800

    update: sync configs for deepseek judger, new script for evaluating

--- eval_sppo.sh
+++ eval_sppo.sh
@@ -1,11 +1,31 @@
 set -x
 set -e
 
-export IS_ALPACA_EVAL_2=False
+export IS_ALPACA_EVAL_2=True
 export HF_HOME="/cephfs/shared/hf_cache"
 
-REFERENCE_MODEL="gpt4_turbo"
+# REFERENCE_MODEL="/cephfs/xukangping/code/alpaca_eval/results/gpt4_1106_preview/model_outputs.json"
 
-# alpaca_eval evaluate_from_model SPPO-Llama-3-Instruct-8B-Iter1-custom --reference_outputs $REFERENCE_MODEL
-# alpaca_eval evaluate_from_model Meta-Llama-3-8B-Instruct --reference_outputs $REFERENCE_MODEL
-alpaca_eval evaluate_from_model Meta-Llama-3-8B-Instruct-custom --reference_outputs $REFERENCE_MODEL
+# MODEL_NAME="SPPO-Llama-3-Instruct-8B-Iter1-custom"
+# MODEL_NAME="Meta-Llama-3-8B-Instruct-custom"
+MODEL_NAMES=(
+    # "SPPO-Llama-3-Instruct-8B-Iter1-custom"
+    # "Meta-Llama-3-8B-Instruct-custom"
+    # "Meta-Llama-3-8B-Instruct"
+    # "gpt4_1106_preview"
+    "SPPO-Llama-3-Instruct-8B-Iter1-table-custom"
+)
+
+JUDGER=weighted_alpaca_eval_deepseek
+# start from None leaderboards
+LAST_TIME_FINAL_MODEL="gpt4_1106_preview"
+
+LEADERBOARD="None"
+if [ ! -z $LAST_TIME_FINAL_MODEL ]; then
+    LEADERBOARD="results/$LAST_TIME_FINAL_MODEL/$JUDGER/leaderboard.csv"
+fi
+
+for MODEL_NAME in "${MODEL_NAMES[@]}"; do
+    alpaca_eval evaluate_from_model $MODEL_NAME --annotators_config $JUDGER --precomputed_leaderboard $LEADERBOARD
+    LEADERBOARD=results/$MODEL_NAME/$JUDGER/leaderboard.csv
+done
