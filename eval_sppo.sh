f223eb279f7273ac40138644b354136933ded29a
xukp20 <xukp20@mails.tsinghua.edu.cn>
2024-08-27 08:41:48 +0800

    update: add iter=2 model

--- eval_sppo.sh
+++ eval_sppo.sh
@@ -3,23 +3,22 @@ set -e
 
 export IS_ALPACA_EVAL_2=True
 export HF_HOME="/cephfs/shared/hf_cache"
+export http_proxy="http://127.0.0.1:7890"
+export https_proxy="http://127.0.0.1:7890"
 
-# REFERENCE_MODEL="/cephfs/xukangping/code/alpaca_eval/results/gpt4_1106_preview/model_outputs.json"
-
-# MODEL_NAME="SPPO-Llama-3-Instruct-8B-Iter1-custom"
-# MODEL_NAME="Meta-Llama-3-8B-Instruct-custom"
 MODEL_NAMES=(
     # "SPPO-Llama-3-Instruct-8B-Iter1-custom"
     # "Meta-Llama-3-8B-Instruct-custom"
     # "Meta-Llama-3-8B-Instruct"
     # "gpt4_1106_preview"
     # "SPPO-Llama-3-Instruct-8B-Iter1-table-custom"
-    SPPO-Llama-3-Instruct-8B-PairRM
+    # SPPO-Llama-3-Instruct-8B-PairRM
+    "SPPO-Llama-3-Instruct-8B-Iter2-custom"
 )
 
 JUDGER=weighted_alpaca_eval_deepseek
 # start from None leaderboards
-LAST_TIME_FINAL_MODEL="SPPO-Llama-3-Instruct-8B-Iter1-table-custom"
+LAST_TIME_FINAL_MODEL="SPPO-Llama-3-Instruct-8B-PairRM"
 
 LEADERBOARD="None"
 if [ ! -z $LAST_TIME_FINAL_MODEL ]; then
