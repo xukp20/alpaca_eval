318569b65bd4ebcf599faaa08e70e40d6e61aaf7
xukp20 <xukp20@mails.tsinghua.edu.cn>
2024-10-01 11:40:03 +0800

    update

--- test_api.sh
+++
@@ -1,5 +0,0 @@
-# curl https://api.gptsapi.net/v1/chat/completions \
-curl https://api1.uiuiapi.com/v1/chat/completions \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer sk-boc9fQCk8STTy8jy840239711dBb4c3dAb6d8319Be747eD2" \
-    -d '{ "model": "gpt-4o-mini", "messages": [ { "role": "user", "content": "Hello!" } ], "logprobs": true, "top_logprobs": 5}'
